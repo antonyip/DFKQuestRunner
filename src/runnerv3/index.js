348669f11c894150ef85a7edb1b93bd68dd5bbdb
Anton Yip <anton_yip@hotmail.com>
2022-02-14 15:41:38 +0800

    a2

--- src/runnerv3/index.js
+++ src/runnerv3/index.js
@@ -510,7 +510,6 @@ async function CheckAndSendGoldMiners(heroesStruct, isPro)
 async function CheckAndSendGardeners(heroesStruct, isPro)
 {
     let minStam = 15;
-    
 
     // too lazy to change struct in config
     let questType = config.quests[5]
