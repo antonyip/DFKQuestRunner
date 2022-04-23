2e6d96027a35c08597a4a634fa517b63fbd3e0ff
Anton Yip <anton_yip@hotmail.com>
2022-04-24 07:54:17 +0800

    refactor

--- src/runnerv3/index.js
+++ src/runnerv3/index.js
@@ -1,18 +1,23 @@
+// external includes
 const { Harmony } = require('@harmony-js/core');
 const {
     ChainID,
     ChainType,
 } = require('@harmony-js/utils');
+const date = require('date-and-time');
 
+// internal includes
 const config = require("./config.json");
 const autils = require("./autils")
-const abi = require("./abi/abi.json")
+const questABI = require("./abi/abi.json")
 const questABI_21apr2022 = require('./abi/questABI_21apr2022.json')
-const GlobalSignOn = true;
-
-const { CheckAndSendStatQuests } = require('./quest_stats');
 const { CompleteQuests } = require('./quest_complete');
+const { CheckAndSendFishers } = require('./quest_fishing');
+const { CheckAndSendForagers } = require('./quest_foraging');
+const { CheckAndSendStatQuests } = require('./quest_stats');
 
+// file local vars
+const GlobalSignOn = true;
 let eBreakCount = 0;
 const eBreakLimit = 10;
 
@@ -27,7 +32,7 @@ const hmy = new Harmony(
 hmy.wallet.addByPrivateKey(process.env.ETH_PRIVATE_KEY);
 
 let questContract = hmy.contracts.createContract(
-    abi,
+    questABI,
     config.questContract,   
     {
         defaultGas: config.gasLimit,
@@ -60,74 +65,6 @@ async function getActiveAccountQuests(latestBlock)
 {
     let results = await questContract_21Apr2022.methods.getAccountActiveQuests(config.wallet).call()
     return results
-}
-
-function fishingPattern(hero1,hero2,hero3,hero4,hero5,hero6,attempts)
-{
-    if (hero1 === 0)
-    {
-        throw new Error("Tried to send create a fishing pattern without heroes")
-        return ""
-    }
-
-    let rv = ""
-    rv += "0x8a2da17b" // start Quest
-    rv += "0000000000000000000000000000000000000000000000000000000000000080" // version i think
-    rv += "000000000000000000000000adffd2a255b3792873a986895c6312e8fbacfc8b" // quest
-    let heroCount = 0;
-    if (hero1 > 0) { ++heroCount; }
-    if (hero2 > 0) { ++heroCount; }
-    if (hero3 > 0) { ++heroCount; }
-    if (hero4 > 0) { ++heroCount; }
-    if (hero5 > 0) { ++heroCount; }
-    if (hero6 > 0) { ++heroCount; }
-
-    rv += autils.intToInput(attempts); // attempts
-    rv += "0000000000000000000000000000000000000000000000000000000000000000" // level
-    rv += autils.intToInput(heroCount); // hero count
-
-    if (hero1 > 0) { rv += autils.intToInput(hero1); }
-    if (hero2 > 0) { rv += autils.intToInput(hero2); }
-    if (hero3 > 0) { rv += autils.intToInput(hero3); }
-    if (hero4 > 0) { rv += autils.intToInput(hero4); }
-    if (hero5 > 0) { rv += autils.intToInput(hero5); }
-    if (hero6 > 0) { rv += autils.intToInput(hero6); }
-
-    return rv;
-}
-
-function foragingPattern(hero1,hero2,hero3,hero4,hero5,hero6,attempts)
-{
-    if (hero1 === 0)
-    {
-        throw new Error("Tried to send create a foraging pattern without heroes")
-        return ""
-    }
-
-    let rv = ""
-    rv += "0x8a2da17b" // start Quest
-    rv += "0000000000000000000000000000000000000000000000000000000000000080" // version
-    rv += "000000000000000000000000b465f4590095dad50fee6ee0b7c6700ac2b04df8" // quest
-    let heroCount = 0;
-    if (hero1 > 0) { ++heroCount; }
-    if (hero2 > 0) { ++heroCount; }
-    if (hero3 > 0) { ++heroCount; }
-    if (hero4 > 0) { ++heroCount; }
-    if (hero5 > 0) { ++heroCount; }
-    if (hero6 > 0) { ++heroCount; }
-
-    rv += autils.intToInput(attempts); // attempts
-    rv += "0000000000000000000000000000000000000000000000000000000000000000" // level
-    rv += autils.intToInput(heroCount); // hero count
-
-    if (hero1 > 0) { rv += autils.intToInput(hero1); }
-    if (hero2 > 0) { rv += autils.intToInput(hero2); }
-    if (hero3 > 0) { rv += autils.intToInput(hero3); }
-    if (hero4 > 0) { rv += autils.intToInput(hero4); }
-    if (hero5 > 0) { rv += autils.intToInput(hero5); }
-    if (hero6 > 0) { rv += autils.intToInput(hero6); }
-
-    return rv;
 }
 
 function goldMiningPattern(hero1,hero2,hero3,hero4,hero5,hero6)
@@ -223,211 +160,6 @@ function gardeningQuestPattern(heroIdInt, poolIdInt) {
     return rv;
 }
 
-async function CheckAndSendFishers(heroesStruct, isPro)
-{
-    let questType = config.quests[0]
-    if (questType.name !== "Fishing")
-    {
-        throw new Error("config index was changed");
-    }
-
-    let minBatch = isPro ? questType.professionHeroes.length : questType.nonProfessionHeroes.length;
-    let maxBatch = 1;
-    minBatch = minBatch > maxBatch ? maxBatch : minBatch;
-    let proStamUsage = 5;
-    let normStamUsage = 7;
-    let minStam = isPro ? questType.proMinStam : questType.normMinStam;
-    let proFishingTries = Math.round(minStam/proStamUsage);
-    let normFishingTries = Math.round(minStam/normStamUsage);
-    let fishingTries = isPro ? proFishingTries : normFishingTries;
-
-    let activeQuesters = heroesStruct.allQuesters
-    let configFishers = isPro ? questType.professionHeroes : questType.nonProfessionHeroes
-
-    //console.log(activeQuesters);
-    //console.log(configFishers);
-    let possibleFishers = configFishers.filter((e) => {
-        return (activeQuesters.indexOf(e) < 0);
-      });
-
-    let FisherPromises = []
-    possibleFishers.forEach(fisher => {
-        FisherPromises.push(questContract.methods.getCurrentStamina(fisher).call())
-    });
-
-    let staminaValues = await Promise.all(FisherPromises)
-    //console.log("fsh stam: " + staminaValues);
-    
-    // Batching fishers. we only take 6. -> next iteration then we go again
-    LocalBatching = []
-    for (let index = 0; index < possibleFishers.length; index++) {
-        const stam = staminaValues[index];
-        if ( stam >= minStam )
-        {
-            LocalBatching.push(possibleFishers[index]);
-        }
-
-        // list full
-        if (LocalBatching.length === maxBatch)
-        {
-            break;
-        }
-    }
-
-    let numHeroesToSend = LocalBatching.length;
-    // fill the last batch up
-    if (LocalBatching.length > 0)
-    {
-        while(LocalBatching.length < maxBatch)
-        {
-            LocalBatching.push(0)
-        }
-    }
-
-    console.log("Fishing Batches" + (isPro ? " (P): " : " (N): ") + LocalBatching)
-
-    // be lazy only send 1 batch for now.. next minute can send another
-    if (numHeroesToSend >= minBatch && minBatch > 0)
-    {
-        // let GasLimit = await hmy.blockchain.estimateGas({ 
-        //     to: config.questContract,
-        //     shardID: 0,
-        //     data: fishingPattern(LocalBatching[0],LocalBatching[1],LocalBatching[2],LocalBatching[3],LocalBatching[4],LocalBatching[5],fishingTries) })
-
-        const txn = hmy.transactions.newTx({
-            to: config.questContract_21Apr2022,
-            value: 0,
-            // gas limit, you can use string
-            gasLimit: config.gasLimit,
-            // send token from shardID
-            shardID: 0,
-            // send token to toShardID
-            toShardID: 0,
-            // gas Price, you can use Unit class, and use Gwei, then remember to use toWei(), which will be transformed to BN
-            gasPrice: config.gasPrice,
-            // tx data
-            data: fishingPattern(LocalBatching[0],LocalBatching[1],LocalBatching[2],LocalBatching[3],LocalBatching[4],LocalBatching[5],fishingTries)
-        });
-          
-        // sign the transaction use wallet;
-        const signedTxn = await hmy.wallet.signTransaction(txn);
-        //  console.log(signedTxn);
-        if (GlobalSignOn === true)
-        {
-            const txnHash = await hmy.blockchain.sendTransaction(signedTxn);
-            console.log("!!! sending the message on the wire !!!");
-            ++eBreakCount;
-            //  console.log(txnHash);
-        }
-        
-        console.log("Sent " + LocalBatching + " on a Fishing Quest")
-    }
-    
-    return;
-}
-async function CheckAndSendForagers(heroesStruct, isPro)
-{
-    // too lazy to change struct in config
-    let questType = config.quests[1]
-    if (questType.name !== "Foraging")
-    {
-        throw new Error("config index was changed");
-    }
-
-    let minBatch = isPro ? questType.professionHeroes.length : questType.nonProfessionHeroes.length;
-    let maxBatch = 1;
-    minBatch = minBatch > maxBatch ? maxBatch : minBatch;
-    let proStamUsage = 5;
-    let normStamUsage = 7;
-    let minStam = isPro ? questType.proMinStam : questType.normMinStam;
-    let proForagingTries = Math.round(minStam/proStamUsage);
-    let normForagingTries = Math.round(minStam/normStamUsage);
-    let foragingTries = isPro ? proForagingTries : normForagingTries;
-
-    let activeQuesters = heroesStruct.allQuesters
-    let configForagers = isPro ? questType.professionHeroes : questType.nonProfessionHeroes
-    //console.log(activeQuesters);
-    //console.log(configForagers);
-    let possibleForagers = configForagers.filter((e) => {
-        return (activeQuesters.indexOf(e) < 0);
-      });
-
-    let ForagerPromises = []
-    possibleForagers.forEach(hero => {
-        ForagerPromises.push(questContract.methods.getCurrentStamina(hero).call())
-    });
-
-    let staminaValues = await Promise.all(ForagerPromises)
-
-    // Batching foragers. we only take 6. -> next iteration then we go again
-    LocalBatching = []
-    for (let index = 0; index < possibleForagers.length; index++) {
-        const stam = staminaValues[index];
-        if ( stam >= minStam )
-        {
-            LocalBatching.push(possibleForagers[index]);
-        }
-
-        // list full
-        if (LocalBatching.length === maxBatch)
-        {
-            break;
-        }
-    }
-
-
-    let numHeroesToSend = LocalBatching.length;
-    // fill the last batch up
-    if (LocalBatching.length > 0)
-    {
-        while(LocalBatching.length < maxBatch)
-        {
-            LocalBatching.push(0)
-        }
-    }
-
-    console.log("Forager Batches" + (isPro ? " (P): " : " (N): ") + LocalBatching)
-
-    // be lazy only send 1 batch for now.. next minute can send another
-    
-    if (numHeroesToSend >= minBatch && minBatch > 0)
-    {
-        // let GasLimit = await hmy.blockchain.estimateGas({ 
-        //     to: config.questContract,
-        //     shardID: 0,
-        //     data: foragingPattern(LocalBatching[0],LocalBatching[1],LocalBatching[2],LocalBatching[3],LocalBatching[4],LocalBatching[5],foragingTries) })
-
-        const txn = hmy.transactions.newTx({
-            to: config.questContract_21Apr2022,
-            value: 0,
-            // gas limit, you can use string
-            gasLimit: config.gasLimit,
-            // send token from shardID
-            shardID: 0,
-            // send token to toShardID
-            toShardID: 0,
-            // gas Price, you can use Unit class, and use Gwei, then remember to use toWei(), which will be transformed to BN
-            gasPrice: new hmy.utils.Unit('30').asGwei().toWei(),
-            // tx data
-            data: foragingPattern(LocalBatching[0],LocalBatching[1],LocalBatching[2],LocalBatching[3],LocalBatching[4],LocalBatching[5],foragingTries)
-        });
-          
-        // sign the transaction use wallet;
-        const signedTxn = await hmy.wallet.signTransaction(txn);
-        //  console.log(signedTxn);
-        if (GlobalSignOn === true)
-        {
-            const txnHash = await hmy.blockchain.sendTransaction(signedTxn);
-            console.log("!!! sending the message on the wire !!!");
-            ++eBreakCount;
-            //  console.log(txnHash);
-        }
-        
-        console.log("Sent " + LocalBatching + " on a " + (isPro ? "professional" : "normal") + "Foraging Quest")
-    }
-    
-    return;
-}
 async function CheckAndSendGoldMiners(heroesStruct, isPro)
 {
     // too lazy to change struct in config
@@ -698,7 +430,7 @@ async function CheckAndSendGardeners(heroesStruct, isPro)
     return;
 }
 
-const date = require('date-and-time');
+
 
 function GetCurrentDateTime()
 {
